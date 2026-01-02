d94120008731324ecdfd32e4954805e6df5452be
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-01-03 00:56:29 +0530

    add some changes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,3 @@
-// import React from "react"
-// import ReactDOM from "react-dom/client"
-// import App from "./App"
-// import { AuthProvider } from "./context/AuthContext"
-// import "./index.css"
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <AuthProvider>
-//       <App />
-//     </AuthProvider>
-//   </React.StrictMode>
-// )
 
 
 import React from "react"
@@ -24,7 +11,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+         <PlayerProvider>
+          <App />
+        </PlayerProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
