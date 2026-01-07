5c242d6073b0ea72cf6b887aa9c321e9354da071
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-01-07 22:48:00 +0530

    some changes in App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import SpotifyLayout from "./layout/SpotifyLayout"
+import { Routes, Route } from "react-router-dom"
+import ProtectedRoute from "./routes/ProtectedRoute"
+
+import Layout from "./layout/Layout"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 import Home from "./pages/Home"
 
 export default function App() {
   return (
-    <SpotifyLayout>
-      <Home />
-    </SpotifyLayout>
+    <Routes>
+      {/* Public Routes */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+
+      {/* Protected Routes */}
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <Layout />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
   )
 }
