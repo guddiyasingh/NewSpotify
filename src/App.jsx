95ae7aed14622c3719fbe86afcad3d4919165e32
Guddiya Singh <guddiyasingh1847@gmail.com>
2026-01-14 08:55:07 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,12 @@ import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Signup from "./pages/Signup"
 import Playlist from "./pages/Playlist"
+import Player from "./components/Player"
+
 
 export default function App() {
   return (
+    <>
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
@@ -17,6 +20,8 @@ export default function App() {
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
     </Routes>
+    <Player />
+    </>
   )
 }
 
